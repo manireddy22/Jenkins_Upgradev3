6aa34321cf0109c652c7bbb5157a61914f754052
manireddy22 <54991261+manireddy22@users.noreply.github.com>
2022-04-03 14:32:03 +0530

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,6 +33,7 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+}
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
